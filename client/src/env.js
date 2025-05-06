9da1f7555aabd279334c5b9f19337cf215f88f59
MohitKumar <mohitsaini25j@gmail.com>
2025-05-06 22:12:19 +0530

    init PR

---
+++ client/src/env.js
@@ -0,0 +1,14 @@
+// const DEV_API = "https://photo-stream-uxkd.onrender.com";
+// const PROD_API = "https://photo-stream-uxkd.onrender.com";
+// const MODE = "dev";
+// const SERVER_DEV_API = "https://photo-stream-uxkd.onrender.com";
+// const SERVER_PROD_API = "https://photo-stream-uxkd.onrender.com";
+
+const DEV_API = "http://localhost:5001";
+const PROD_API = "http://localhost:5001";
+const MODE = "dev";
+const SERVER_DEV_API = "http://localhost:5001";
+const SERVER_PROD_API = "http://localhost:5001";
+
+export { DEV_API, PROD_API, MODE, SERVER_DEV_API, SERVER_PROD_API };
+
