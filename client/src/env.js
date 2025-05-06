cd0944d6359d6edff2bb4ba8d778f73808a0c827
MohitKumar <mohitsaini25j@gmail.com>
2025-05-06 22:17:13 +0530

    updated endpoints

--- client/src/env.js
+++ client/src/env.js
@@ -4,11 +4,11 @@
 // const SERVER_DEV_API = "https://photo-stream-uxkd.onrender.com";
 // const SERVER_PROD_API = "https://photo-stream-uxkd.onrender.com";
 
-const DEV_API = "http://localhost:5001";
-const PROD_API = "http://localhost:5001";
+const DEV_API = "https://photo-stream-y6lh.onrender.com";
+const PROD_API = "https://photo-stream-y6lh.onrender.com";
 const MODE = "dev";
-const SERVER_DEV_API = "http://localhost:5001";
-const SERVER_PROD_API = "http://localhost:5001";
+const SERVER_DEV_API = "https://photo-stream-y6lh.onrender.com";
+const SERVER_PROD_API = "https://photo-stream-y6lh.onrender.com";
 
 export { DEV_API, PROD_API, MODE, SERVER_DEV_API, SERVER_PROD_API };
 
